dac29619ca747d8b1d30ba2d288696dfa1a14693
Alexander Levashov <alevash1@gmail.com>
2019-04-05 20:11:19 +0300

    Create chats

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,20 +1,28 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    logger.info "Subscribed to RoomChannel"
+    logger.info "Subscribed to RoomChannel, roomId: #{params[:roomId]}"
 
-    stream_from 'room_channel'
+    @room = Room.find(params[:roomId])
+
+    stream_from "room_channel_#{@room.id}"
+
+    speak('message' => '* * * joined the room * * *')
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
     logger.info "Unsubscribed from RoomChannel"
+
+    speak('message' => '* * * left the room * * *')
   end
 
   def speak(data)
     # byebug
     logger.info "RoomChannel, speak: #{data.inspect}"
 
-    ActionCable.server.broadcast 'room_channel', message: 'Hello from server!'
+    MessageService.new(
+      body: data['message'], user: current_user, room: @room
+    ).perform
   end
 end
